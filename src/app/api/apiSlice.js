cd9f7f0b6f3732cf8289beda21a42db92dd958ac
minhbn3107 <109571044+minhbn3107@users.noreply.github.com>
2023-11-20 20:30:41 +0700

    fix link

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -3,7 +3,7 @@ import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
     baseUrl:
-        "https://technotes-api-kfik.onrender.com/" || "http://localhost:3500",
+        "https://technotes-api-kfik.onrender.com" || "http://localhost:3500",
     credentials: "include",
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token;
